6749982b6ad61413c1e388e7fe176f5994e690f3
Brian Groth <groth.brian@gmail.com>
2025-09-02 22:13:07 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,6 @@
+import TwoMonthCalendarApp from "./two-month-calendar.js";
+
+// Render your component into #root
+const rootEl = document.getElementById("root");
+const root = ReactDOM.createRoot(rootEl);
+root.render(React.createElement(TwoMonthCalendarApp));
